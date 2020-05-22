0a28ea3417c0ce3d16c77ea2edfcc10415906d37
David Paul <enlightenedpie@gmail.com>
2020-05-22 11:35:58 -0500

    made some changes

--- config.js
+++ config.js
@@ -1,37 +1,32 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Eventually', // <title>
-  manifestName: 'Eventually',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: 'The Shaw Group | Pipes and More Pipes!!!', // <title>
+  manifestName: 'Shaw Pipes',
+  manifestShortName: 'ShawPipes', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   // social
-  heading: 'Eventually',
+  heading: 'Shaw Pipes',
   subHeading:
-    "A simple template for telling the world when you'll launch your next big thing.",
+    "We love laying pipe, and our pipes our huge. Email us and we'll let you know when we're ready to lay some pipe for you.",
   socialLinks: [
-    {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
-    },
     {
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
+      url: 'https://twitter.com/twitter',
     },
     {
       icon: 'fa-facebook',
       name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
+      url: 'https://facebook.com/facebook',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'mailto:info@gmail.com',
     },
   ],
 };
